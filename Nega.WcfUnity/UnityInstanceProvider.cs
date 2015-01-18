dfee4d92f7bab23a0d5f57c8fc2baa19f4036430
oceannut <oceannut@163.com>
2015-01-18 20:21:57 +0800

    startup

---
+++ Nega.WcfUnity/UnityInstanceProvider.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Description;
+
+using Microsoft.Practices.Unity;
+
+namespace Nega.WcfUnity
+{
+    public class UnityInstanceProvider : IInstanceProvider, IContractBehavior
+    {
+
+        private readonly IUnityContainer container;
+
+        public UnityInstanceProvider(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+
+            this.container = container;
+        }
+
+        #region IInstanceProvider Members
+
+        public object GetInstance(InstanceContext instanceContext,
+          Message message)
+        {
+            return this.GetInstance(instanceContext);
+        }
+
+        public object GetInstance(InstanceContext instanceContext)
+        {
+            return this.container.Resolve(
+              instanceContext.Host.Description.ServiceType);
+        }
+
+        public void ReleaseInstance(InstanceContext instanceContext,
+          object instance)
+        {
+            this.container.Teardown(instance);
+        }
+
+        #endregion
+
+        #region IContractBehavior Members
+
+        public void AddBindingParameters(
+          ContractDescription contractDescription,
+          ServiceEndpoint endpoint,
+          BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(
+          ContractDescription contractDescription,
+          ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+        }
+
+        public void ApplyDispatchBehavior(
+          ContractDescription contractDescription,
+          ServiceEndpoint endpoint,
+          DispatchRuntime dispatchRuntime)
+        {
+            dispatchRuntime.InstanceProvider = this;
+        }
+
+        public void Validate(
+          ContractDescription contractDescription,
+          ServiceEndpoint endpoint)
+        {
+        }
+
+        #endregion
+
+
+    }
+
+}
---
+++ Nega.WcfUnity/UnityServiceHost.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+
+namespace Nega.WcfUnity
+{
+    public class UnityServiceHost : ServiceHost
+    {
+
+        public UnityServiceHost(IUnityContainer container,
+            Type serviceType, params Uri[] baseAddresses)
+            : base(serviceType, baseAddresses)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+
+            foreach (var cd in this.ImplementedContracts.Values)
+            {
+                cd.Behaviors.Add(new UnityInstanceProvider(container));
+            }
+        }
+
+
+    }
+}

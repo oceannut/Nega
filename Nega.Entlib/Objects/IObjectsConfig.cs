bafb02f599bea43b56a648be35244622f5e81afa
oceannut <oceannut@163.com>
2015-07-12 18:02:09 +0800

    objects registry

---
+++ Nega.Entlib/Objects/IObjectsConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+
+namespace Nega.Entlib
+{
+
+    public interface IObjectsConfig
+    {
+
+        void Config(IUnityContainer container);
+
+    }
+
+}
---
+++ Nega.Entlib/Objects/ObjectsFactory.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nega.Entlib
+{
+
+    public class ObjectsFactory
+    {
+
+        private static readonly ObjectsFactory soloInstance = new ObjectsFactory();
+        public static ObjectsFactory SoloInstance
+        {
+            get { return soloInstance; }
+        }
+
+        private List<IObjectsConfig> configs;
+
+        private ObjectsFactory()
+        {
+            configs = new List<IObjectsConfig>();
+        }
+
+        public void AddConfig(IObjectsConfig config)
+        {
+            if (!this.configs.Contains(config))
+            {
+                this.configs.Add(config);
+            }
+        }
+
+        public void Registrate(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException();
+            }
+            foreach (var config in this.configs)
+            {
+                config.Config(container);
+            }
+        }
+
+    }
+
+}
